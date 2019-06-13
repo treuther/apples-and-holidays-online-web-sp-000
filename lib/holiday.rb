4badf273c68ec22c60151d38a8909ffebcc1b503
Tracy Reuther <tracyreuther@gmail.com>
2019-06-13 22:35:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
       end
   end
 end
-all_supplies_in_holidays(holiday_hash)
+holiday_hash
 
 def all_holidays_with_bbq(holiday_supplies)
   # return an array of holiday names (as symbols) where supply lists
