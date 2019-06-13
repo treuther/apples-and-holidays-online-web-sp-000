940066f051512ce01625ff7c5aba8429c2d553bb
Tracy Reuther <tracyreuther@gmail.com>
2019-06-13 23:50:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,7 @@ def all_holidays_with_bbq(holiday_supplies)
     holiday.map do |each_holiday, decoration|
       answer = []
       if decoration.include?("BBQ")
-        answer << each_holiday
+        answer << each_holiday.compact
       end
     end
   end
