996ad4b54649e7842da50ba7ce7790dad1e89668
Tracy Reuther <tracyreuther@gmail.com>
2019-06-13 23:06:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,10 +78,10 @@ end
 def all_holidays_with_bbq(holiday_supplies)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_supplies.map do |season, holiday|
-    holiday.map do |each_holiday, decoration|
+  holiday_supplies.each do |season, holiday|
+    holiday.each do |each_holiday, decoration|
       if decoration.include?("BBQ")
-        holiday
+        holiday.to_s.chomp
       end
     end
   end
