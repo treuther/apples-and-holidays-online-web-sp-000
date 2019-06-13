357beaf63c7ddc6cdf5b02032887b3ada0d70764
Tracy Reuther <tracyreuther@gmail.com>
2019-06-13 22:45:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,5 +81,8 @@ def all_holidays_with_bbq(holiday_supplies)
   holiday_supplies.each do |season, holiday|
     holiday.collect do |each_holiday, decoration|
       if decoration == "BBQ"
-        return each_holiday
+        each_holiday
+      end
+    end
+  end
 end
